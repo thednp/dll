62377714144af61c2f0204b75966e1fd009d9920
thednp <thednp@users.noreply.github.com>
2017-02-18 04:32:52 +0200

    More code cleanup.

--- dll.js
+++ dll.js
@@ -15,16 +15,13 @@
 }(this, function () {
   "use strict";
 
-  // utility
-  var elementInViewport = function(element) { //check element is in viewport
-    var rect = element.getBoundingClientRect();
-    return ( 
-      rect.top >= 0
-      && rect.left >= 0
-      && rect.top <= (window.innerHeight || document.documentElement.clientHeight)
-    )
-  };
-  
+  // strings
+  var getAttribute = 'getAttribute',
+      dataSRC = 'data-src',
+      srcSelector = '['+dataSRC+']',
+      querySelectorAll = 'querySelectorAll',
+      length = 'length';
+
   // DLL DEFINITION
   // ==============
   return function(element,callbackFn){
@@ -37,13 +34,9 @@
 
     var self = this, 
 
-      // strings
-      getAttribute = 'getAttribute',
-      dataSRC = 'data-src',
-      srcSelector = '['+dataSRC+']',
 
       // element's src attribute
-      elementSRC = element && element[getAttribute]('data-src') || null, //element has own data-src attribute
+      elementSRC = element && element[getAttribute](dataSRC) || null, //element has own data-src attribute
   
       // private methods
       loader = function(elementImage, imageCallback) {
@@ -54,37 +47,37 @@
           } else {
             elementImage.style.backgroundImage = 'url("'+src+'")';
           }  
-          elementImage.removeAttribute('data-src');
+          elementImage.removeAttribute(dataSRC);
           if (imageCallback) { imageCallback() }
         }
         img.src = src
       },
       getElements = function() { //we get images of a given object or itself
-        var q, imgs = [], items = element.querySelectorAll(srcSelector);
+        var queue, imgs = [], items = element[querySelectorAll](srcSelector);
         if ( elementSRC && !items) {
-          q = [element]    
+          queue = [element];
         } else if ( !elementSRC && items ) {
-          q = items
+          queue = items;
         } else if ( elementSRC && items ){
-          q = items;
+          queue = items;
           /* put element first in line, it may have a bigger image 
-          * to load than all childNodes combined */
+           * to load than all childNodes combined */
           // imgs.push(element);
           imgs.unshift(element);                 
         } else if ( !elementSRC && !items ){
-          q = document.querySelectorAll(srcSelector)
+          queue = document[querySelectorAll](srcSelector);
         }
         
-        for (var i = 0; i < q.length; i++) { imgs.push(q[i]) }
+        for (var i = 0; i < queue[length]; i++) { imgs.push(queue[i]) }
         
         return imgs;
       };
 
     // init
-    if (elementSRC||element.querySelector(srcSelector) !== null) {
+    if (elementSRC || element.querySelector(srcSelector) !== null) {
       var images = getElements();
-      for (var i = 0; i < images.length; i++) {
-        if ( i === images.length-1 && callbackFn) {
+      for (var i = 0; i < images[length]; i++) {
+        if ( i === images[length]-1 && callbackFn) {
           loader(images[i],callbackFn);
         } else {
           loader(images[i])
